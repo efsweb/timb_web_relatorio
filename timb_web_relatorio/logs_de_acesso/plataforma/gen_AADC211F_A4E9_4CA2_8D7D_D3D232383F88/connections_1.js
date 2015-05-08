57f6e2833c70f35f5969e521cddf86db1f9f4e23
mikeoliveira <mikeoliveira@outlook.com>
2015-05-08 15:07:47 -0300

    exclusao de pastas desnecessarias - PLATAFORMA

--- timb_web_relatorio/logs_de_acesso/plataforma/gen_AADC211F_A4E9_4CA2_8D7D_D3D232383F88/connections_1.js
+++
@@ -1,2 +0,0 @@
-
-var rt=xcelsius.runtime.RuntimeAPI.getCurrentRuntime();rt.setDashboardStatus(xcelsius.utils.DashboardStatusUtil.CONNS_INIT_BEGIN);rt.setDashboardStatus(xcelsius.utils.DashboardStatusUtil.CONNS_INIT_END);rt=undefined;
