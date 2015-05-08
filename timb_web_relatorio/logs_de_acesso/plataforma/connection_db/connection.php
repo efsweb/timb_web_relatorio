46d0d3d51d5b98d2e99c89f12e1226e62c2814d1
mikeoliveira <mikeoliveira@outlook.com>
2015-05-08 12:01:03 -0300

    alteracoes na connection.php executando a proc

--- timb_web_relatorio/logs_de_acesso/plataforma/connection_db/connection.php
+++ timb_web_relatorio/logs_de_acesso/plataforma/connection_db/connection.php
@@ -30,7 +30,14 @@ class connect_mysql{
     $sql = 'select COUNT(tipo_acesso) from mb_timb_log_acesso where tipo_acesso LIKE "Android" ';
     $rs = mysql_query($sql);
     $this->result_ANDROID = mysql_fetch_row($rs);
+    
+    // PARAMETROS PARA CHAMAR A FUNCAO QUE EXECUTA PROCEDURE
+    $procedure = 'sp_timb_fe_relatorio';
+    $parametro = 'acesso';
+    $result_proc = $this->call_procedure($procedure,$parametro);
   }
+
+
 
   /**
    * Invoca a procedure a ser utilizada
@@ -38,20 +45,20 @@ class connect_mysql{
    * @param  [type] $params      String com o valor dos parametros da SP separados por ","
    * @return [type]              Retorna a resposta da SP do banco
    */
-  public function call_precedure($proc_string, $params){
-      $pieces = explode(",", $params);
-      $params_str = str_repeat('?,', count($pieces) - 1) . '?';
-      $stmt = $this->link->prepare("call ".$proc_string."(". $params_str .")");
-      
-      for ($i=0; $i < sizeof($pieces); $i++) {
-        $val = $pieces[$i];
-        $val = substr($val, 0, -1);
-        $val = substr($val, 1, strlen($val)-1);
-        $pos = $i + 1;
-        $stmt->bindValue($pos, "$val" , PDO::PARAM_STR);
+  public function call_procedure($proc_string, $params){
+
+      $mysqli = new mysqli($this->servidor, $this->usuario, $this->senha, $this->banco);
+     
+      $DBH = $mysqli->stmt_init();
+
+      if($DBH->prepare("CALL ".$proc_string."('". $params."')")){
+
+        $stmt = $mysqli->query("CALL ".$proc_string."('". $params ."')");
+        $rs = mysqli_fetch_all($stmt);
+        
       }
-      $rs = $stmt->execute();
-      $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        var_dump($rs); exit;  
+    
       return json_encode($result);
   }
 }
