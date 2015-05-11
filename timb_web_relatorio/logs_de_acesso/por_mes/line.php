7144ab5f3951d0d8852e488f81274f64cb566efa
mikeoliveira <mikeoliveira@outlook.com>
2015-05-11 16:16:44 -0300

    VARIAVEIS DINAMICAS - GRAFICO DE LINHAS

--- timb_web_relatorio/logs_de_acesso/por_mes/line.php
+++ timb_web_relatorio/logs_de_acesso/por_mes/line.php
@@ -13,12 +13,68 @@
 
 			$vl_meses = array();
 			$meses = array("Jan","Fev","Mar","Abr","Mai","Jun","Jul", "Ago", "Set", "Out","Nov", "Dez");
+			$exibe_meses = array(); //ORDENA OS MESES DE ACORDO COM O MES ATUAL
 
 			require_once '../config/connection.php';
 
 			$connect_mysql = new connect_mysql();
 
-			$vl_meses = $connect_mysql->result_proc;	
+			$vl_meses = $connect_mysql->result_proc;
+			//COLUNAS DO BANCO DE DADOS	-> VÁ EM CONFIG/CONNECTION.PHP E DE UM VAR_DUMP PARA ENTENDER AS VARIAVEIS QUE PERCORREM O CODIGO.
+
+			$mes_atual = intval(date('m'));//VALOR DO MES ATUAL EM NUMERO
+
+			//VALORES RESPECTIVOS AS 12 MESES A SEREM EXIBIDOS NO GRAFICO
+
+			//ANO ATUAL
+			$ano_atual['1']	= array(); //ANO ATUAL
+
+			//ANO ANTERIOR 
+			$ano_ant['0']	= array(); //ANO ANTERIOR 
+
+			$array_ios = array();
+			$result_array_ios = array();
+
+			//PERCORRE A DATA ARMAZENANDO APENAS OS MESES QUE SE PASSARAM DO ANO ATUAL (EXEMPLO: JAN - ABR = 1,2,3,4)
+			for ($i = $mes_atual+1 ; $i >= 2; $i--) { 
+				array_push($ano_atual['1'],$i);		//ADICIONA OS VALORES A UM ARRAY ($ano_atual)
+				$exibe_meses[] = $meses[$i-2]; //REALIZA A ORDENACAO DE ACORDO COM O MES ATUAL
+
+			}
+			
+			for($i = 0 ; $i <= $mes_atual; $i++){
+				
+				//CRIO UM ARRAY COM OS VALORES NOS INDICES CORRETOS DE CADA MES E ANO REGISTRADO EM CADA DISPOSITIVO NO BANCO 
+				if (isset($vl_meses['1'][$ano_atual['1'][$i]])){
+					$array_ano_atual['1'][$ano_atual['1'][$i]] =  $vl_meses['1'][$ano_atual['1'][$i]];
+					$result_array_ios = array_push($array_ios, $vl_meses['1'][$ano_atual['1'][$i]]) ;					
+				}
+
+			}
+			
+			if($mes_atual != 12){ //VERIFICA SE REALMENTE É NECESSARIO TER CONTAGEM DOS MESES DO ANO ANTERIOR
+				
+				$j = $mes_atual+2; //ESSA SOMA É PARA SE IGUALAR A MATRIZ DO BANCO PARA A CONTAGEM DOS MESES 
+
+				for ($j ; $j <= 13; $j++) {
+					array_push($ano_ant['0'],$j);	//ADICIONA OS VALORES A UM ARRAY ($ano_ant)
+					$exibe_meses[] = $meses[$j-2]; //REALIZA A ORDENACAO DE ACORDO COM O MES ATUAL
+				}
+				var_dump($exibe_meses);
+				$mes_atual++;
+				
+				for($j = 0 ; $j <= $mes_atual; $j++){
+					
+					//CRIO UM ARRAY COM OS VALORES NOS INDICES CORRETOS DE CADA MES E ANO REGISTRADO EM CADA DISPOSITIVO NO BANCO 
+					$array_ano_ant['0'][$ano_ant['0'][$j]] =  $vl_meses['0'][$ano_ant['0'][$j]]; 
+					$result_array_ios = array_push($array_ios, $vl_meses['0'][$ano_ant['0'][$j]] );
+
+				}
+			}
+
+			$array_grafico = array_merge($array_ano_ant,$array_ano_atual);
+
+			var_dump($array_ios);
 
 			?>
 
@@ -39,18 +95,18 @@
 		    	//STRING DOS MESES
 		        labels : 
 		        [
-		        	<?php echo '"'.$meses[0].'"'; ?>,
-		        	<?php echo '"'.$meses[1].'"'; ?>,
-		        	<?php echo '"'.$meses[2].'"'; ?>,
-		        	<?php echo '"'.$meses[3].'"'; ?>,
-		        	<?php echo '"'.$meses[4].'"'; ?>,
-		        	<?php echo '"'.$meses[5].'"'; ?>,
-		        	<?php echo '"'.$meses[6].'"'; ?>,
-		        	<?php echo '"'.$meses[7].'"'; ?>,
-		        	<?php echo '"'.$meses[8].'"'; ?>,
-		        	<?php echo '"'.$meses[9].'"'; ?>,
-		        	<?php echo '"'.$meses[10].'"'; ?>,
-		        	<?php echo '"'.$meses[11].'"'; ?>
+		        	<?php echo '"'.$exibe_meses[0].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[1].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[2].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[3].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[4].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[5].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[6].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[7].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[8].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[9].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[10].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[11].'"'; ?>,
 		        ],
 		        datasets : [
 		            {
@@ -91,21 +147,21 @@
 
 		function lineChartAndroid() {
 		    var data = {
-		    	//STRING DOS MESES
+		    	//STRING DOS exibe_meses
 				labels : 
 		        [
-		        	<?php echo '"'.$meses[0].'"'; ?>,
-		        	<?php echo '"'.$meses[1].'"'; ?>,
-		        	<?php echo '"'.$meses[2].'"'; ?>,
-		        	<?php echo '"'.$meses[3].'"'; ?>,
-		        	<?php echo '"'.$meses[4].'"'; ?>,
-		        	<?php echo '"'.$meses[5].'"'; ?>,
-		        	<?php echo '"'.$meses[6].'"'; ?>,
-		        	<?php echo '"'.$meses[7].'"'; ?>,
-		        	<?php echo '"'.$meses[8].'"'; ?>,
-		        	<?php echo '"'.$meses[9].'"'; ?>,
-		        	<?php echo '"'.$meses[10].'"'; ?>,
-		        	<?php echo '"'.$meses[11].'"'; ?>
+		        	<?php echo '"'.$exibe_meses[0].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[1].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[2].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[3].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[4].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[5].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[6].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[7].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[8].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[9].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[10].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[11].'"'; ?>,
 		        ],
 		        datasets : [
 		            {
@@ -149,18 +205,18 @@
 		    	//STRING DOS MESES
 		        labels : 
 		        [
-		        	<?php echo '"'.$meses[0].'"'; ?>,
-		        	<?php echo '"'.$meses[1].'"'; ?>,
-		        	<?php echo '"'.$meses[2].'"'; ?>,
-		        	<?php echo '"'.$meses[3].'"'; ?>,
-		        	<?php echo '"'.$meses[4].'"'; ?>,
-		        	<?php echo '"'.$meses[5].'"'; ?>,
-		        	<?php echo '"'.$meses[6].'"'; ?>,
-		        	<?php echo '"'.$meses[7].'"'; ?>,
-		        	<?php echo '"'.$meses[8].'"'; ?>,
-		        	<?php echo '"'.$meses[9].'"'; ?>,
-		        	<?php echo '"'.$meses[10].'"'; ?>,
-		        	<?php echo '"'.$meses[11].'"'; ?>
+		        	<?php echo '"'.$exibe_meses[0].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[1].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[2].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[3].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[4].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[5].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[6].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[7].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[8].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[9].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[10].'"'; ?>,
+		        	<?php echo '"'.$exibe_meses[11].'"'; ?>,
 		        ],
 		        datasets : [
 		            {
